62cb8a900ad23d50d4962223f719f60ee481c56f
Cezar Cruz <cezarcruz@outlook.com>
2023-06-26 22:36:21 -0300

    adding policy

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,11 @@
-const headerNavLinks = [
+type HeaderNavLinks = { href: string; title: string }
+
+const headerNavLinks: HeaderNavLinks[] = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   //{ href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/privacy-policy', title: 'Terms' },
 ]
 
 export default headerNavLinks
