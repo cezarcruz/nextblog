750cf6dc4727ea94d40ffc85e7bf296a6a28cf2a
Cezar Cruz <cezarcruz@outlook.com>
2023-06-29 23:52:38 -0300

    adding new tracker (#19)
    * adding new tracker
    
    * fixing url

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -23,7 +23,7 @@ const siteMetadata = {
     // supports plausible, simpleAnalytics, umami or googleAnalytics
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    umamiWebsiteId: 'a41f10ac-c8d8-4651-ae8e-a403e1058c66', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: 'G-JTD4SN4ZV5', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
