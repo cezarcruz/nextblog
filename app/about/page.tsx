481dc8cce611288d350d0ed56cc7c6ce9367f59d
Cezar Cruz <cezarcruz22@gmail.com>
2024-04-19 16:34:29 -0300

    fixing privacy policy

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,8 @@
+import AuthorLayout from '@/layouts/AuthorLayout'
+import { genPageMetadata } from 'app/seo'
 import { Authors, allAuthors } from 'contentlayer/generated'
 import { MDXLayoutRenderer } from 'pliny/mdx-components'
-import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
-import { genPageMetadata } from 'app/seo'
 
 export const metadata = genPageMetadata({ title: 'About' })
 
@@ -14,6 +14,12 @@ export default function Page() {
     <>
       <AuthorLayout content={mainContent}>
         <MDXLayoutRenderer code={author.body.code} />
+        <p>Seja bem vindo ao meu Blog.</p>
+        <p>
+          Meu nome é Cezar e desenvolvo para Web utilizando Java e Javascript, criei esse blog para
+          compartilhar algumas coisas que vou aprendendo.
+        </p>
+        <p>Então, seja bem vindo e aproveite a viagem.</p>
       </AuthorLayout>
     </>
   )
